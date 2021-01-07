33b917b6cf87bdb06a7c3156bfe181037b1d85f0
Farah Gustafson <farah.gus@hotmail.com>
2021-01-07 18:14:47 -0500

    added post service

--- src/Services/Movie-Service.js
+++ src/Services/Movie-Service.js
@@ -8,6 +8,20 @@ const movieService = {
                     ? res.json().then(e => Promise.reject(e))
                     : res.json())
     },
+
+    postMovie(id, title, overview, genre_id, release_date) {
+        return fetch(`http://localhost:8000/myMovies`, {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json',
+                //authorization
+            },
+            body: JSON.stringify(id, title, overview, genre_id, release_date),
+        })
+            .then(res => {
+                (!res.ok) ? res.json().then(e => Promise.reject(e)) : res.json()
+            })
+    }
 }
 
 export default movieService;
